43a6040151e98e9834c9b6d37414d92c345a07b8
praise adedoyin <adedoyinpraise408@gmail.com>
2022-06-23 10:30:32 -0700

    Javascript
    Introduction to javascript

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Javascript.github.io
+This is a simple javascript program that inputs simple information about me on the screen.
  
---
+++ script.js
@@ -0,0 +1,10 @@
+const about_me = {
+    name: 'Adedoyin Praise',
+    height: '6.2 inches',
+    country: 'Nigeria' ,
+}
+alert(
+    `Hi!
+     My name is ${about_me.name} , I am ${about_me.height} tall
+    and I am from ${about_me.country}. `
+);
